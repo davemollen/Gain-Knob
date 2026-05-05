01f606383b2c01c6a140ca16a8550566c71abafa
Dave Mollen <davemollen@gmail.com>
2026-05-05 19:04:00 +0200

    Rename to prevent_key_event_propagation

--- src/lib.rs
+++ src/lib.rs
@@ -126,9 +126,9 @@ impl Plugin for GainKnob {
                     move |window_handler, _setter, _window| {
                         let component = window_handler.component();
 
-                        // Pass the keyboard_input_is_enabled state to the window handler
-                        window_handler.set_keyboard_input_is_enabled(
-                            component.get_keyboard_input_is_enabled(),
+                        // Pass the prevent_key_event_propagation state to the window handler
+                        window_handler.set_prevent_key_event_propagation(
+                            component.get_prevent_key_event_propagation(),
                         );
 
                         // Plugin -> UI: sync gain knob position
